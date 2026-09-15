182a1d6c4203f865e2c4641da4e38e77b29274eb
janghr2 <wkdgpfls09932@gmail.com>
2026-09-15 10:51:37 +0900

    변수 수정

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 console.log("hello js")
 // 주석
 // 변수: 값을 저장하는 이름을 가진 공간 
-const username = "장혜린";
+// const username = "장혜린";
+let username = "장혜린";
 
 console. log("username", username)
+
+username = "김도영";
+
+
+console. log("username", username)
+
+const num = 10;//숫자
+console.log("🚀 ~ num:", num, "num type", typeof num)
+
+const text = "안녕하세요" 
+console.log("🚀 ~ text:", text)
+
+const isDarkMode = true; 
+console.log("🚀 ~ isDarkMode:", isDarkMode)
+console.log("🚀 ~ isDarkMode:", typeof isDarkMode)
+
+const empty = null;
+console.log("🚀 ~ empty:", empty, typeof empty)
+
+let userage;
+ console.log("🚀 ~ userage:", userage)
+ 
+
+
+
+
+//console. log("num", num)
